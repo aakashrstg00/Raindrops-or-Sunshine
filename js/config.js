d25cff68af795f6138458b05cfdb688a587cbf0a
aakashrstg00 <1998aakashrastogi@gmail.com>
2017-03-09 19:12:01 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 var urls = {
-    locPollURL: 'http://api.openweathermap.org/data/2.5/weather?q='
+    locPollURL: 'https://api.openweathermap.org/data/2.5/weather?q='
     , apikey: '857d06912a6bba329cc9256d13fc8890'
 };
